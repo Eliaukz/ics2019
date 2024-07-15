052e9d447f8e205b510be6f7930d4f7d88198c0b
tracer-ics2019 <tracer@njuics.org>
2024-07-15 21:32:56 +0800

    >  compile 181220000 yzd Linux DESKTOP-6NT9BEJ 5.15.153.1-microsoft-standard-WSL2 #1 SMP Fri Mar 29 23:14:13 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  21:32:56 up 56 min,  1 user,  load average: 0.30, 0.19, 0.11 4bb32c523cfc1e6f63e1d131a7c1e3eb97f1ec83

--- nemu/src/isa/riscv32/exec/all-instr.h
+++ nemu/src/isa/riscv32/exec/all-instr.h
@@ -1,17 +1,28 @@
 #include "cpu/exec.h"
 
+
+// compute.c
+make_EHelper(lui);
+make_EHelper(auipc);
+make_EHelper(imm);
+make_EHelper(reg);
+
+// control.c
+make_EHelper(jal);
+make_EHelper(jalr);
+make_EHelper(branch);
+
+// special.c
 make_EHelper(inv);
 make_EHelper(nemu_trap);
-//load store
+
+
+//load store  ldst.c
 make_EHelper(ld);
 make_EHelper(st);
 make_EHelper(lh);
 make_EHelper(lb);
-make_EHelper(lui);
-make_EHelper(auipc);
-make_EHelper(jal);
-make_EHelper(jalr);
-make_EHelper(branch);
-make_EHelper(imm);
-make_EHelper(reg);
+
+
+
 make_EHelper(sys);
